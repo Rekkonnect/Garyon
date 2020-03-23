031ae90b89bb33bc77a702249f73bbe435809769
AlFas <AlFasGD@users.noreply.github.com>
2020-03-23 21:29:24 +0200

    Add missing exception type

---
+++ Garyon.Benchmarking/Exceptions/InstructionSetBenchmarkException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Garyon.Benchmarking.Exceptions
+{
+    public class InstructionSetBenchmarkException : BenchmarkException
+    {
+        public InstructionSetBenchmarkException() : base("The system does not support the required instruction set to test this function.") { }
+        public InstructionSetBenchmarkException(string message) : base(message) { }
+        public InstructionSetBenchmarkException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
