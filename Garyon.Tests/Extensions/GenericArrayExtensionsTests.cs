f210448c3240efb93d25a4d0cddcd26a89317378
AlFas <AlFasGD@users.noreply.github.com>
2020-03-23 21:28:17 +0200

    Fix and add tests for generic array extensions

---
+++ Garyon.Tests/Extensions/GenericArrayExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using Garyon.Extensions;
+using NUnit.Framework;
+
+namespace Garyon.Tests.Extensions
+{
+    public class GenericArrayExtensionsTests
+    {
+        [Test]
+        public void IsArrayOfType()
+        {
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[], object>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[,], object>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[,,], object>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[,,,], object>());
+
+            Assert.IsFalse(GenericArrayExtensions.IsArrayOfType<object[], int>());
+            Assert.IsFalse(GenericArrayExtensions.IsArrayOfType<object[], short>());
+            Assert.IsFalse(GenericArrayExtensions.IsArrayOfType<short[], int>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<int[], int>());
+
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfByte<byte[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfInt16<short[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfInt32<int[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfInt64<long[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfSByte<sbyte[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfUInt16<ushort[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfUInt32<uint[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfUInt64<ulong[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfSingle<float[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfDouble<double[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfDecimal<decimal[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfChar<char[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfBoolean<bool[]>());
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfString<string[]>());
+        }
+        [Test]
+        public void IsJaggedArrayOfType()
+        {
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[][], object>(2));
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[,][], object>(2));
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[,][,], object>(2));
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[,][,,,], object>(2));
+
+            Assert.IsFalse(GenericArrayExtensions.IsArrayOfType<object[][][], object>(2));
+            Assert.IsFalse(GenericArrayExtensions.IsArrayOfType<object[][][][], object>(2));
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[][][][], object>(4));
+            Assert.IsTrue(GenericArrayExtensions.IsArrayOfType<object[][][][][], object>(6));
+        }
+    }
+}
---
+++ Garyon.Tests/Resources/AssertionHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace Garyon.Tests.Resources
+{
+    public static class AssertionHelpers
+    {
+        /// <summary>Ignores the current test, throwing the appropriate exception and displays a message related to unsupported instruction set.</summary>
+        public static void UnsupportedInstructionSet()
+        {
+            Assert.Ignore("The system does not support the required instruction set to test this function.");
+        }
+    }
+}
