5448b79636783fcd51a793d5c56333b8f92808eb
AlFas <AlFasGD@users.noreply.github.com>
2020-03-24 01:11:07 +0200

    Add throw helper class

---
+++ Garyon/Exceptions/ThrowHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Garyon.Exceptions
+{
+    public static class ThrowHelper
+    {
+        public static void Throw<T>()
+            where T : Exception, new()
+        {
+            throw new T();
+        }
+    }
+}
