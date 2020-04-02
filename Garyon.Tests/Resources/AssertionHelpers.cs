e72499cbee987eee7e3fda9ef75c7408da75f201
AlFas <AlFasGD@users.noreply.github.com>
2020-04-03 02:52:05 +0300

    Add more helpful functions for tests

--- Garyon.Tests/Resources/AssertionHelpers.cs
+++ Garyon.Tests/Resources/AssertionHelpers.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.IO;
 using System.Text;
 using NUnit.Framework;
+using System.Diagnostics;
 
 namespace Garyon.Tests.Resources
 {
@@ -13,6 +14,37 @@ namespace Garyon.Tests.Resources
         public static void UnsupportedInstructionSet()
         {
             Assert.Ignore("The system does not support the required instruction set to test this function.");
+        }
+
+        /// <summary>Asserts whether the provided array is a valid array in the memory without causing issues.</summary>
+        /// <param name="a">The array to test.</param>
+        public static void TestArray(Array a)
+        {
+            Assert.IsTrue(a is object && a.Length != 0);
+        }
+        /// <summary>Asserts whether the provided arrays are valid arrays in the memory without causing issues.</summary>
+        /// <param name="arrays">The arrays to test.</param>
+        public static void TestArrays(params Array[] arrays)
+        {
+            foreach (Array a in arrays)
+                TestArray(a);
+        }
+    }
+
+    public static class DebugAssertionHelpers
+    {
+        /// <summary>Asserts whether the provided array is a valid array in the memory without causing issues.</summary>
+        /// <param name="a">The array to test.</param>
+        public static void TestArray(Array a)
+        {
+            Debug.Assert(a is object && a.Length != 0);
+        }
+        /// <summary>Asserts whether the provided arrays are valid arrays in the memory without causing issues.</summary>
+        /// <param name="arrays">The arrays to test.</param>
+        public static void TestArrays(params Array[] arrays)
+        {
+            foreach (Array a in arrays)
+                TestArray(a);
         }
     }
 }
