a9bbfc6ad0aca0b91ed7a3deaa9915e79c1135c7
AlFas <AlFasGD@users.noreply.github.com>
2020-04-25 18:55:29 +0300

    Add more throw helper functions

--- Garyon/Exceptions/ThrowHelper.cs
+++ Garyon/Exceptions/ThrowHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace Garyon.Exceptions
@@ -6,6 +7,7 @@ namespace Garyon.Exceptions
     /// <summary>A helper class providing tools for throwing exceptions. It should be preferred to use in code that demands optimization.</summary>
     public static class ThrowHelper
     {
+        #region Generic Throwers
         /// <summary>Throws a new exception of the type <typeparamref name="T"/>.</summary>
         /// <typeparam name="T">The exception type to throw.</typeparam>
         [MethodImpl(MethodImplOptions.NoInlining)]
@@ -32,13 +34,54 @@ namespace Garyon.Exceptions
         public static void Throw<T>(string message, Exception innerException)
             where T : Exception
         {
-            var constructor = typeof(T).GetConstructor(new Type[] { typeof(string), typeof(Exception) });
-            throw constructor.Invoke(new object[] { message, innerException }) as T;
+            Throw<T, Exception>(message, innerException);
+        }
+        /// <summary>Throws a new aggregate exception of the type <typeparamref name="T"/>.</summary>
+        /// <typeparam name="T">The exception type to throw.</typeparam>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerExceptions">The exceptions that are the cause of the current exception.</param>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowAggregate<T>(string message, params Exception[] innerExceptions)
+            where T : AggregateException
+        {
+            Throw<T, IEnumerable<Exception>>(message, innerExceptions);
+        }
+        /// <summary>Throws a new aggregate exception of the type <typeparamref name="T"/>.</summary>
+        /// <typeparam name="T">The exception type to throw.</typeparam>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerExceptions">The exceptions that are the cause of the current exception.</param>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowAggregate<T>(string message, IEnumerable<Exception> innerExceptions)
+            where T : AggregateException
+        {
+            Throw<T, IEnumerable<Exception>>(message, innerExceptions);
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Throw<TException, TInner>(string message, TInner inner)
+            where TException : Exception
+        {
+            var constructor = typeof(TException).GetConstructor(new Type[] { typeof(string), typeof(TInner) });
+            throw constructor.Invoke(new object[] { message, inner }) as TException;
+        }
+        #endregion
+
+        #region Instance Throwers
         /// <summary>Throws an exception whose instance is already created.</summary>
         /// <param name="e">The exception instance to throw.</param>
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static void Throw(Exception e) => throw e;
+        #endregion
+
+        #region Standarized Exception Throwers
+        /// <summary>Throws a new <seealso cref="AggregateException"/>.</summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerExceptions">The exceptions that are the cause of the current exception.</param>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowAggregate(string message, params Exception[] innerExceptions)
+        {
+            throw new AggregateException(message, innerExceptions);
+        }
+        #endregion
     }
 }
