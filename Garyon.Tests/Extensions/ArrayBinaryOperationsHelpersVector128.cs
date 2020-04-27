f56300b4fcc643d5c853d31bcb1ac7cab14c5831
AlFas <AlFasGD@users.noreply.github.com>
2020-04-27 17:23:51 +0300

    Add tests for Vector128

---
+++ Garyon.Tests/Extensions/ArrayBinaryOperationsHelpersVector128.cs
@@ -0,0 +1,285 @@
+using Garyon.QualityControl.Extensions;
+using NUnit.Framework;
+using static Garyon.Functions.PointerHelpers.SIMDPointerBinaryOperations;
+using static Garyon.Tests.Resources.AssertionHelpers;
+
+namespace Garyon.Tests.Extensions
+{
+    public class ArrayBinaryOperationsHelpersVector128 : ArrayManipulationExtensionsQualityControlAsset
+    {
+        private const byte mask = 11;
+
+        #region AND
+        [Test]
+        public unsafe void ANDByteArray()
+        {
+            fixed (byte* o = OriginalByteArray)
+            fixed (byte* t = TargetByteArray)
+                if (!ANDArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalByteArray[i] & mask) == TargetByteArray[i]);
+        }
+        [Test]
+        public unsafe void ANDInt16Array()
+        {
+            fixed (short* o = OriginalInt16Array)
+            fixed (short* t = TargetInt16Array)
+                if (!ANDArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt16Array[i] & mask) == TargetInt16Array[i]);
+        }
+        [Test]
+        public unsafe void ANDInt32Array()
+        {
+            fixed (int* o = OriginalInt32Array)
+            fixed (int* t = TargetInt32Array)
+                if (!ANDArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt32Array[i] & mask) == TargetInt32Array[i]);
+        }
+        [Test]
+        public unsafe void ANDInt64Array()
+        {
+            fixed (long* o = OriginalInt64Array)
+            fixed (long* t = TargetInt64Array)
+                if (!ANDArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt64Array[i] & mask) == TargetInt64Array[i]);
+        }
+        [Test]
+        public unsafe void ANDSByteArray()
+        {
+            fixed (sbyte* o = OriginalSByteArray)
+            fixed (sbyte* t = TargetSByteArray)
+                if (!ANDArrayVector128Generic(o, t, (sbyte)mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalSByteArray[i] & mask) == TargetSByteArray[i]);
+        }
+        [Test]
+        public unsafe void ANDUInt16Array()
+        {
+            fixed (ushort* o = OriginalUInt16Array)
+            fixed (ushort* t = TargetUInt16Array)
+                if (!ANDArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt16Array[i] & mask) == TargetUInt16Array[i]);
+        }
+        [Test]
+        public unsafe void ANDUInt32Array()
+        {
+            fixed (uint* o = OriginalUInt32Array)
+            fixed (uint* t = TargetUInt32Array)
+                if (!ANDArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt32Array[i] & mask) == TargetUInt32Array[i]);
+        }
+        [Test]
+        public unsafe void ANDUInt64Array()
+        {
+            fixed (ulong* o = OriginalUInt64Array)
+            fixed (ulong* t = TargetUInt64Array)
+                if (!ANDArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt64Array[i] & mask) == TargetUInt64Array[i]);
+        }
+        #endregion
+
+        #region OR
+        [Test]
+        public unsafe void ORByteArray()
+        {
+            fixed (byte* o = OriginalByteArray)
+            fixed (byte* t = TargetByteArray)
+                if (!ORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalByteArray[i] | mask) == TargetByteArray[i]);
+        }
+        [Test]
+        public unsafe void ORInt16Array()
+        {
+            fixed (short* o = OriginalInt16Array)
+            fixed (short* t = TargetInt16Array)
+                if (!ORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt16Array[i] | mask) == TargetInt16Array[i]);
+        }
+        [Test]
+        public unsafe void ORInt32Array()
+        {
+            fixed (int* o = OriginalInt32Array)
+            fixed (int* t = TargetInt32Array)
+                if (!ORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt32Array[i] | mask) == TargetInt32Array[i]);
+        }
+        [Test]
+        public unsafe void ORInt64Array()
+        {
+            fixed (long* o = OriginalInt64Array)
+            fixed (long* t = TargetInt64Array)
+                if (!ORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt64Array[i] | mask) == TargetInt64Array[i]);
+        }
+        [Test]
+        public unsafe void ORSByteArray()
+        {
+            fixed (sbyte* o = OriginalSByteArray)
+            fixed (sbyte* t = TargetSByteArray)
+                if (!ORArrayVector128Generic(o, t, (sbyte)mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalSByteArray[i] | mask) == TargetSByteArray[i]);
+        }
+        [Test]
+        public unsafe void ORUInt16Array()
+        {
+            fixed (ushort* o = OriginalUInt16Array)
+            fixed (ushort* t = TargetUInt16Array)
+                if (!ORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt16Array[i] | mask) == TargetUInt16Array[i]);
+        }
+        [Test]
+        public unsafe void ORUInt32Array()
+        {
+            fixed (uint* o = OriginalUInt32Array)
+            fixed (uint* t = TargetUInt32Array)
+                if (!ORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt32Array[i] | mask) == TargetUInt32Array[i]);
+        }
+        [Test]
+        public unsafe void ORUInt64Array()
+        {
+            fixed (ulong* o = OriginalUInt64Array)
+            fixed (ulong* t = TargetUInt64Array)
+                if (!ORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt64Array[i] | mask) == TargetUInt64Array[i]);
+        }
+        #endregion
+
+        #region XOR
+        [Test]
+        public unsafe void XORByteArray()
+        {
+            fixed (byte* o = OriginalByteArray)
+            fixed (byte* t = TargetByteArray)
+                if (!XORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalByteArray[i] ^ mask) == TargetByteArray[i]);
+        }
+        [Test]
+        public unsafe void XORInt16Array()
+        {
+            fixed (short* o = OriginalInt16Array)
+            fixed (short* t = TargetInt16Array)
+                if (!XORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt16Array[i] ^ mask) == TargetInt16Array[i]);
+        }
+        [Test]
+        public unsafe void XORInt32Array()
+        {
+            fixed (int* o = OriginalInt32Array)
+            fixed (int* t = TargetInt32Array)
+                if (!XORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt32Array[i] ^ mask) == TargetInt32Array[i]);
+        }
+        [Test]
+        public unsafe void XORInt64Array()
+        {
+            fixed (long* o = OriginalInt64Array)
+            fixed (long* t = TargetInt64Array)
+                if (!XORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalInt64Array[i] ^ mask) == TargetInt64Array[i]);
+        }
+        [Test]
+        public unsafe void XORSByteArray()
+        {
+            fixed (sbyte* o = OriginalSByteArray)
+            fixed (sbyte* t = TargetSByteArray)
+                if (!XORArrayVector128Generic(o, t, (sbyte)mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalSByteArray[i] ^ mask) == TargetSByteArray[i]);
+        }
+        [Test]
+        public unsafe void XORUInt16Array()
+        {
+            fixed (ushort* o = OriginalUInt16Array)
+            fixed (ushort* t = TargetUInt16Array)
+                if (!XORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt16Array[i] ^ mask) == TargetUInt16Array[i]);
+        }
+        [Test]
+        public unsafe void XORUInt32Array()
+        {
+            fixed (uint* o = OriginalUInt32Array)
+            fixed (uint* t = TargetUInt32Array)
+                if (!XORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt32Array[i] ^ mask) == TargetUInt32Array[i]);
+        }
+        [Test]
+        public unsafe void XORUInt64Array()
+        {
+            fixed (ulong* o = OriginalUInt64Array)
+            fixed (ulong* t = TargetUInt64Array)
+                if (!XORArrayVector128Generic(o, t, mask, ArrayLength))
+                    UnsupportedInstructionSet();
+
+            for (int i = 0; i < ArrayLength; i++)
+                Assert.IsTrue((OriginalUInt64Array[i] ^ mask) == TargetUInt64Array[i]);
+        }
+        #endregion
+    }
+}
