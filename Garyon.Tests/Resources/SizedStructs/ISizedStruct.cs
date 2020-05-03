7acbae9046e333eaa175bcf533aa4515262a6b72
AlFas <AlFasGD@users.noreply.github.com>
2020-05-04 01:13:56 +0300

    Add sized structs

---
+++ Garyon.Tests/Resources/SizedStructs/ISizedStruct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Garyon.Tests.Resources.SizedStructs
+{
+    /// <summary>Represents a sized struct.</summary>
+    public interface ISizedStruct { }
+
+    /// <summary>Represents a sized struct.</summary>
+    /// <typeparam name="T">The type of the struct, provided to <seealso cref="IEquatable{T}"/> for the Equals function.</typeparam>
+    public interface ISizedStruct<T> : ISizedStruct, IEquatable<T> { }
+}
