106cb9d5661e6348ddcbbbf82d5ca505428de0d5
AlFas <AlFasGD@users.noreply.github.com>
2020-07-19 00:06:51 +0300

    Add flexible dictionaries

---
+++ Garyon/DataStructures/FlexibleDictionary.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Garyon.DataStructures
+{
+    /// <summary>Represents a flexible dictionary, which is a dictionary that allows indexing or setting values on non-existing keys without throwing exceptions.</summary>
+    /// <typeparam name="TKey">The type of the keys that are being added to the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values that are being paired with the keys in the dictionary.</typeparam>
+    public class FlexibleDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>, IDictionary<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> dictionary;
+
+        #region Hidden Interface Properties
+        bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly => false;
+        #endregion
+
+        public int Count => dictionary.Count;
+
+        public ICollection<TKey> Keys => dictionary.Keys;
+        public ICollection<TValue> Values => dictionary.Values;
+
+        /// <summary>Initializes a new instance of the <seealso cref="FlexibleDictionary{TKey, TValue}"/> class with the default initial capacity (16).</summary>
+        public FlexibleDictionary() : this(16) { }
+        /// <summary>Initializes a new instance of the <seealso cref="FlexibleDictionary{TKey, TValue}"/> class.</summary>
+        /// <param name="capacity">The capacity of the dictionary.</param>
+        public FlexibleDictionary(int capacity)
+        {
+            dictionary = new Dictionary<TKey, TValue>(capacity);
+        }
+        /// <summary>Initializes a new instance of the <seealso cref="FlexibleDictionary{TKey, TValue}"/> class.</summary>
+        /// <param name="collection">The collection to initialize the dictionary from. Each item in the provided collection is added as a key and is mapped to the default value of the <typeparamref name="TValue"/> type.</param>
+        public FlexibleDictionary(IEnumerable<TKey> collection)
+            : this()
+        {
+            foreach (var v in collection)
+                Add(v);
+        }
+        /// <summary>Initializes a new instance of the <seealso cref="FlexibleDictionary{TKey, TValue}"/> class out of another <seealso cref="FlexibleDictionary{TKey, TValue}"/> instance.</summary>
+        /// <param name="other">The other <seealso cref="FlexibleDictionary{TKey, TValue}"/> whose key-value pairs to copy.</param>
+        public FlexibleDictionary(FlexibleDictionary<TKey, TValue> other)
+            : this(other.Count)
+        {
+            foreach (var kvp in other.dictionary)
+                dictionary.Add(kvp.Key, kvp.Value);
+        }
+
+        public virtual void Add(TKey key, TValue value = default) => dictionary.TryAdd(key, value);
+        public virtual void Add(KeyValuePair<TKey, TValue> kvp) => dictionary.Add(kvp.Key, kvp.Value);
+        public virtual bool Remove(TKey key) => dictionary.Remove(key);
+        public void Clear() => dictionary.Clear();
+
+        /// <summary>Clones this <seealso cref="FlexibleDictionary{TKey, TValue}"/> and adds all its keys to the resulting instance.</summary>
+        /// <returns>The cloned instance containing the same key-value pairs.</returns>
+        public virtual FlexibleDictionary<TKey, TValue> Clone() => new FlexibleDictionary<TKey, TValue>(this);
+
+        public bool ContainsKey(TKey key) => dictionary.ContainsKey(key);
+        public bool TryGetValue(TKey key, out TValue value) => dictionary.TryGetValue(key, out value);
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => dictionary.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => dictionary.GetEnumerator();
+
+        #region Hidden Interface Methods
+        bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item) => (dictionary as IDictionary<TKey, TValue>).Contains(item);
+        void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => (dictionary as IDictionary<TKey, TValue>).CopyTo(array, arrayIndex);
+        bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item) => (dictionary as IDictionary<TKey, TValue>).Remove(item);
+        #endregion
+
+        public virtual TValue this[TKey key]
+        {
+            get
+            {
+                if (!ContainsKey(key))
+                    dictionary.Add(key, default);
+                return dictionary[key];
+            }
+            set
+            {
+                if (!ContainsKey(key))
+                    dictionary.Add(key, value);
+                else
+                    dictionary[key] = value;
+            }
+        }
+    }
+}
---
+++ Garyon/DataStructures/FlexibleListDictionary.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace Garyon.DataStructures
+{
+    /// <summary>Represents a flexible dictionary that maps a key to a list of objects.</summary>
+    /// <typeparam name="TKey">The type of the key to index the dictionary contents by.</typeparam>
+    /// <typeparam name="TObject">The type of objects to be stored in the list of objects each key is mapped to.</typeparam>
+    public class FlexibleListDictionary<TKey, TObject> : FlexibleDictionary<TKey, List<TObject>>
+    {
+        /// <summary>Initializes a new instance of the <seealso cref="FlexibleListDictionary{TKey, TObject}"/> class with the default initial capacity (16).</summary>
+        public FlexibleListDictionary() : base() { }
+        /// <summary>Initializes a new instance of the <seealso cref="FlexibleListDictionary{TKey, TObject}"/> class.</summary>
+        /// <param name="capacity">The capacity of the dictionary.</param>
+        public FlexibleListDictionary(int capacity) : base(capacity) { }
+        /// <summary>Initializes a new instance of the <seealso cref="FlexibleListDictionary{TKey, TObject}"/> class.</summary>
+        /// <param name="collection">The collection to initialize the dictionary from. Each item in the provided collection is added as a key and is mapped to the default value of the <typeparamref name="TValue"/> type.</param>
+        public FlexibleListDictionary(IEnumerable<TKey> collection) : base(collection) { }
+        /// <summary>Initializes a new instance of the <seealso cref="FlexibleListDictionary{TKey, TObject}"/> class out of another <seealso cref="FlexibleListDictionary{TKey, TObject}"/> instance.</summary>
+        /// <param name="other">The other <seealso cref="FlexibleListDictionary{TKey, TObject}"/> whose key-value pairs to copy.</param>
+        public FlexibleListDictionary(FlexibleListDictionary<TKey, TObject> other)
+            : base()
+        {
+            foreach (var kvp in other)
+                Add(kvp.Key, new List<TObject>(kvp.Value));
+        }
+
+        /// <summary>Adds a new key entry and initializes the list it maps to with only the provided object.</summary>
+        /// <param name="key">The key to add to the dictionary.</param>
+        /// <param name="value">The single object that will be added to the list the provided key will map to.</param>
+        public virtual void Add(TKey key, TObject value = default) => base.Add(key, new List<TObject> { value });
+        /// <summary>Adds a new key entry and initializes the list it maps to with only the provided object.</summary>
+        /// <param name="kvp">The <seealso cref="KeyValuePair{TKey, TValue}"/> containing the key and the value within the list that will be added to the dictionary.</param>
+        public virtual void Add(KeyValuePair<TKey, TObject> kvp) => Add(kvp.Key, kvp.Value);
+
+        public override FlexibleDictionary<TKey, List<TObject>> Clone() => new FlexibleListDictionary<TKey, TObject>(this);
+
+        /// <summary>Attempts to get a value within the mapped list from a provided key and with an index within the list.</summary>
+        /// <param name="key">The key to look for in the dictionary.</param>
+        /// <param name="index">The index of the desired value within the list the provided key maps to.</param>
+        /// <param name="value">The value that was requested.</param>
+        /// <returns><see langword="true"/> if the key existed in the dictionary and the index was within the list's bounds, otherwise <see langword="false"/>.</returns>
+        public bool TryGetValue(TKey key, int index, out TObject value)
+        {
+            value = default;
+            if (TryGetValue(key, out var list))
+                return false;
+            if (index >= list.Count)
+                return false;
+            value = list[index];
+            return true;
+        }
+
+        public override List<TObject> this[TKey key]
+        {
+            get
+            {
+                if (!ContainsKey(key))
+                    base.Add(key, new List<TObject>());
+                return base[key];
+            }
+            set
+            {
+                if (!ContainsKey(key))
+                    base.Add(key, value);
+                else
+                    base[key] = value;
+            }
+        }
+    }
+}
