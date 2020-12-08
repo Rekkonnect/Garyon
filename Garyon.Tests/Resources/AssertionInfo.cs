fa40301542ab16848a8f39d1ae2e528a5d1d4754
AlFas <AlFasGD@users.noreply.github.com>
2020-12-08 22:03:13 +0200

    Maintain Garyon.Tests.Resources

---
+++ Garyon.Tests/Resources/AssertionInfo.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Garyon.Tests.Resources
+{
+    /// <summary>Contains information about an assertion process.</summary>
+    /// <typeparam name="T">The type of the values that will be used in the assertion.</typeparam>
+    public class AssertionInfo<T>
+    {
+        /// <summary>The expected value.</summary>
+        public T Expected { get; set; }
+        /// <summary>The actual value.</summary>
+        public T Actual { get; set; }
+
+        /// <summary>Initializes a new instance of the <seealso cref="AssertionInfo{T}"/> class.</summary>
+        public AssertionInfo() { }
+        /// <summary>Initializes a new instance of the <seealso cref="AssertionInfo{T}"/> class.</summary>
+        /// <param name="expected">The expected value.</param>
+        /// <param name="actual">The actual value.</param>
+        public AssertionInfo(T expected, T actual)
+        {
+            Expected = expected;
+            Actual = actual;
+        }
+
+        /// <summary>Asserts the equality of the expected and the actual values.</summary>
+        public void AssertEquality()
+        {
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
--- Garyon.Tests/Resources/DebugAssertionHelpers.cs
+++ Garyon.Tests/Resources/DebugAssertionHelpers.cs
@@ -1,21 +1,15 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
+using System.Diagnostics;
 
 namespace Garyon.Tests.Resources
 {
-    public static class AssertionHelpers
+    public static class DebugAssertionHelpers
     {
-        /// <summary>Ignores the current test, throwing the appropriate exception and displays a message related to unsupported instruction set.</summary>
-        public static void UnsupportedInstructionSet()
-        {
-            Assert.Ignore("The system does not support the required instruction set to test this function.");
-        }
-
         /// <summary>Asserts whether the provided array is a valid array in the memory without causing issues.</summary>
         /// <param name="a">The array to test.</param>
         public static void TestArray(Array a)
         {
-            Assert.IsTrue(a is object && a.Length != 0);
+            Debug.Assert(a is object && a.Length != 0);
         }
         /// <summary>Asserts whether the provided arrays are valid arrays in the memory without causing issues.</summary>
         /// <param name="arrays">The arrays to test.</param>
