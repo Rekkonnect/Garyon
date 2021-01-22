21c1231a42ed61e7293499f5ee5ec60135552471
AlFas <AlFasGD@users.noreply.github.com>
2021-01-22 16:10:05 +0200

    Further abstract the ITree and ITreeNode interfaces for general purposes

--- Garyon/DataStructures/ITree.cs
+++ Garyon/DataStructures/ITree.cs
@@ -4,15 +4,8 @@ namespace Garyon.DataStructures
 {
     /// <summary>Represents a tree.</summary>
     /// <typeparam name="TValue">The type of the elements the tree nodes store.</typeparam>
-    /// <typeparam name="TTree">The type of the tree that this type is used in.</typeparam>
-    /// <typeparam name="TTreeNode">The type of the tree nodes that are used in the <typeparamref name="TTree"/> type.</typeparam>
-    public interface ITree<TValue, TTree, TTreeNode> : ICollection<TValue>
-        where TTree : ITree<TValue, TTree, TTreeNode>
-        where TTreeNode : ITreeNode<TValue, TTree, TTreeNode>
+    public interface ITree<TValue> : ICollection<TValue>
     {
-        /// <summary>Gets or sets the root node.</summary>
-        TTreeNode Root { get; set; }
-
         #region Properties
         /// <summary>Gets the height of this tree. It is equal to the height of the root node.</summary>
         int Height { get; }
@@ -21,33 +14,17 @@ namespace Garyon.DataStructures
         #endregion
 
         #region Nodes
-        /// <summary>Gets a child node within this tree that has the specified value.</summary>
-        /// <param name="value">The value of the child tree node to find.</param>
-        /// <returns>The <seealso cref="TreeNode{T}"/> with the specified value, if found; otherwise <see langword="false"/>.</returns>
-        TTreeNode GetNode(TValue value);
-
         /// <summary>Removes a child within this tree.</summary>
         /// <param name="value">The value of the child to remove from the node.</param>
         /// <returns><see langword="true"/> if the node was successfully removed, otherwise <see langword="false"/>.</returns>
         bool RemoveNode(TValue value);
-        /// <summary>Removes a child within this tree.</summary>
-        /// <param name="childToRemove">The child to remove from the node.</param>
-        /// <returns><see langword="true"/> if the node was successfully removed, otherwise <see langword="false"/>.</returns>
-        bool RemoveNode(TTreeNode childToRemove);
         /// <summary>Removes nodes within this tree that contain the specified values.</summary>
         /// <param name="values">The values of the children to remove.</param>
         void RemoveNodes(IEnumerable<TValue> values);
-        /// <summary>Removes nodes within a subtree with root as the specified node that contain the specified values.</summary>
-        /// <param name="node">The node which is the root of the subtree in which to remove the nodes at.</param>
-        /// <param name="values">The values of the children to remove.</param>
-        void RemoveNodes(TTreeNode node, IEnumerable<TValue> values);
 
         /// <summary>Clears the children of a node with the specified value, that is within this tree.</summary>
         /// <param name="value">The value of the parent node whose children to clear. The node must be within this tree.</param>
         void ClearChildren(TValue value);
-        /// <summary>Clears the children of a node within this tree.</summary>
-        /// <param name="node">The parent node whose children to clear. The node must be within this tree.</param>
-        void ClearChildren(TTreeNode node);
         #endregion
 
         #region Traversal
@@ -60,6 +37,46 @@ namespace Garyon.DataStructures
         /// <summary>Traverses the tree from the root using level-order.</summary>
         /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded values.</returns>
         IEnumerable<TValue> TraverseLevelOrder();
+        #endregion
+
+        /// <summary>Gets the tree view of this tree node as a subtree.</summary>
+        /// <param name="childIndent">The length of the indentation for each subsequent child.</param>
+        /// <returns>The tree view.</returns>
+        string GetTreeView(int childIndent);
+    }
+
+    /// <summary>Represents a tree.</summary>
+    /// <typeparam name="TValue">The type of the elements the tree nodes store.</typeparam>
+    /// <typeparam name="TTree">The type of the tree that this type is used in.</typeparam>
+    /// <typeparam name="TTreeNode">The type of the tree nodes that are used in the <typeparamref name="TTree"/> type.</typeparam>
+    public interface ITree<TValue, TTree, TTreeNode> : ITree<TValue>
+        where TTree : ITree<TValue, TTree, TTreeNode>
+        where TTreeNode : ITreeNode<TValue, TTree, TTreeNode>
+    {
+        /// <summary>Gets or sets the root node.</summary>
+        TTreeNode Root { get; set; }
+
+        #region Nodes
+        /// <summary>Gets a child node within this tree that has the specified value.</summary>
+        /// <param name="value">The value of the child tree node to find.</param>
+        /// <returns>The <seealso cref="TreeNode{T}"/> with the specified value, if found; otherwise <see langword="false"/>.</returns>
+        TTreeNode GetNode(TValue value);
+
+        /// <summary>Removes a child within this tree.</summary>
+        /// <param name="childToRemove">The child to remove from the node.</param>
+        /// <returns><see langword="true"/> if the node was successfully removed, otherwise <see langword="false"/>.</returns>
+        bool RemoveNode(TTreeNode childToRemove);
+        /// <summary>Removes nodes within a subtree with root as the specified node that contain the specified values.</summary>
+        /// <param name="node">The node which is the root of the subtree in which to remove the nodes at.</param>
+        /// <param name="values">The values of the children to remove.</param>
+        void RemoveNodes(TTreeNode node, IEnumerable<TValue> values);
+
+        /// <summary>Clears the children of a node within this tree.</summary>
+        /// <param name="node">The parent node whose children to clear. The node must be within this tree.</param>
+        void ClearChildren(TTreeNode node);
+        #endregion
+
+        #region Traversal
         /// <summary>Traverses the tree from the root using pre-order.</summary>
         /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded nodes.</returns>
         IEnumerable<TTreeNode> TraversePreOrderNodes();
@@ -70,10 +87,5 @@ namespace Garyon.DataStructures
         /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded nodes.</returns>
         IEnumerable<TTreeNode> TraverseLevelOrderNodes();
         #endregion
-
-        /// <summary>Gets the tree view of this tree node as a subtree.</summary>
-        /// <param name="childIndent">The length of the indentation for each subsequent child.</param>
-        /// <returns>The tree view.</returns>
-        string GetTreeView(int childIndent);
     }
 }
--- Garyon/DataStructures/ITreeNode.cs
+++ Garyon/DataStructures/ITreeNode.cs
@@ -4,23 +4,10 @@ namespace Garyon.DataStructures
 {
     /// <summary>Represents a tree node, which is a component that is contained within a tree.</summary>
     /// <typeparam name="TValue">The type of the value that is stored in the node.</typeparam>
-    /// <typeparam name="TTree">The type of the tree that this type is used in.</typeparam>
-    /// <typeparam name="TTreeNode">The type of the tree nodes that are used in the <typeparamref name="TTree"/> type.</typeparam>
-    public interface ITreeNode<TValue, TTree, TTreeNode> : IEnumerable<TValue>
-        where TTree : ITree<TValue, TTree, TTreeNode>
-        where TTreeNode : ITreeNode<TValue, TTree, TTreeNode>
+    public interface ITreeNode<TValue> : IEnumerable<TValue>
     {
         /// <summary>The value of the node.</summary>
         TValue Value { get; set; }
-
-        /// <summary>Gets or sets the base tree that contains this node.</summary>
-        TTree BaseTree { get; set; }
-
-        /// <summary>Gets or sets the parent node of this tree node.</summary>
-        TTreeNode Parent { get; set; }
-        /// <summary>Gets or sets the children nodes of this tree.</summary>
-        /// <value>The new list of nodes to set the children to.</value>
-        List<TTreeNode> Children { get; set; }
 
         /// <summary>Gets the count of children of this node; prefer calling this property instead of getting the count directly from the <see cref="Children"/> property.</summary>
         int ChildrenCount { get; }
@@ -44,23 +31,10 @@ namespace Garyon.DataStructures
         #endregion
 
         #region Nodes
-        /// <summary>Gets a direct child of this node that has the specified value.</summary>
-        /// <param name="value">The value of the direct child node to find.</param>
-        /// <returns>The <typeparamref name="TTreeNode"/> with the specified value, if found; otherwise <see langword="false"/>.</returns>
-        TTreeNode GetChild(TValue value);
-        /// <summary>Gets a node from the subtree with this tree node as a root node that has the specified value.</summary>
-        /// <param name="value">The value of the tree node to find.</param>
-        /// <returns>The <typeparamref name="TTreeNode"/> with the specified value, if found; otherwise <see langword="false"/>.</returns>
-        TTreeNode GetNode(TValue value);
-
         /// <summary>Removes a node that has the specified value. This includes any node that is within the subtree with this tree node as the root.</summary>
         /// <param name="value">The value of the node to remove from this node's children list.</param>
         /// <returns><see langword="true"/> if the child was successfully removed from this node's children list, otherwise <see langword="false"/>.</returns>
         bool RemoveNode(TValue value);
-        /// <summary>Removes a node that has the specified value. This includes any node that is within the subtree with this tree node as the root.</summary>
-        /// <param name="node">The node to remove from this node's children list.</param>
-        /// <returns><see langword="true"/> if the child was successfully removed from this node's children list, otherwise <see langword="false"/>.</returns>
-        bool RemoveNode(TTreeNode node);
         /// <summary>Removes nodes that have the specified values. This includes any node that is within the subtree with this tree node as the root.</summary>
         /// <param name="values">The values of the child nodes to remove from this node's children list.</param>
         /// <returns>The number of nodes from the list that were removed.</returns>
@@ -72,10 +46,62 @@ namespace Garyon.DataStructures
 
         /// <summary>Removes a child from this node's children list.</summary>
         /// <param name="childToRemove">The child to remove from this node's children list.</param>
-        bool RemoveChild(TTreeNode childToRemove);
+        bool RemoveChild(TValue childToRemove);
+        #endregion
+
+        #region Traversal
+        /// <summary>Traverses the subtree with this node as a root using pre-order.</summary>
+        /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded values.</returns>
+        IEnumerable<TValue> TraversePreOrder();
+        /// <summary>Traverses the subtree with this node as a root using post-order.</summary>
+        /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded values.</returns>
+        IEnumerable<TValue> TraversePostOrder();
+        /// <summary>Traverses the subtree with this node as a root using level-order.</summary>
+        /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded values.</returns>
+        IEnumerable<TValue> TraverseLevelOrder();
+        #endregion
+
+        /// <summary>Gets the tree view of this tree node as a subtree.</summary>
+        /// <param name="childIndent">The length of the indentation for each subsequent child.</param>
+        /// <returns>The tree view.</returns>
+        string GetTreeView(int childIndent = 4);
+    }
+
+    /// <summary>Represents a tree node, which is a component that is contained within a tree.</summary>
+    /// <typeparam name="TValue">The type of the value that is stored in the node.</typeparam>
+    /// <typeparam name="TTree">The type of the tree that this type is used in.</typeparam>
+    /// <typeparam name="TTreeNode">The type of the tree nodes that are used in the <typeparamref name="TTree"/> type.</typeparam>
+    public interface ITreeNode<TValue, TTree, TTreeNode> : ITreeNode<TValue>
+        where TTree : ITree<TValue, TTree, TTreeNode>
+        where TTreeNode : ITreeNode<TValue, TTree, TTreeNode>
+    {
+        /// <summary>Gets or sets the base tree that contains this node.</summary>
+        TTree BaseTree { get; set; }
+
+        /// <summary>Gets or sets the parent node of this tree node.</summary>
+        TTreeNode Parent { get; set; }
+        /// <summary>Gets or sets the children nodes of this tree.</summary>
+        /// <value>The new list of nodes to set the children to.</value>
+        List<TTreeNode> Children { get; set; }
+
+        #region Nodes
+        /// <summary>Gets a direct child of this node that has the specified value.</summary>
+        /// <param name="value">The value of the direct child node to find.</param>
+        /// <returns>The <typeparamref name="TTreeNode"/> with the specified value, if found; otherwise <see langword="false"/>.</returns>
+        TTreeNode GetChild(TValue value);
+        /// <summary>Gets a node from the subtree with this tree node as a root node that has the specified value.</summary>
+        /// <param name="value">The value of the tree node to find.</param>
+        /// <returns>The <typeparamref name="TTreeNode"/> with the specified value, if found; otherwise <see langword="false"/>.</returns>
+        TTreeNode GetNode(TValue value);
+
+        /// <summary>Removes a node that has the specified value. This includes any node that is within the subtree with this tree node as the root.</summary>
+        /// <param name="node">The node to remove from this node's children list.</param>
+        /// <returns><see langword="true"/> if the child was successfully removed from this node's children list, otherwise <see langword="false"/>.</returns>
+        bool RemoveNode(TTreeNode node);
+
         /// <summary>Removes a child from this node's children list.</summary>
         /// <param name="childToRemove">The child to remove from this node's children list.</param>
-        bool RemoveChild(TValue childToRemove);
+        bool RemoveChild(TTreeNode childToRemove);
 
         /// <summary>Makes this node a child of another tree node.</summary>
         /// <param name="other">The other tree node that will become this node's parent.</param>
@@ -91,15 +117,6 @@ namespace Garyon.DataStructures
 
         #region Traversal
         /// <summary>Traverses the subtree with this node as a root using pre-order.</summary>
-        /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded values.</returns>
-        IEnumerable<TValue> TraversePreOrder();
-        /// <summary>Traverses the subtree with this node as a root using post-order.</summary>
-        /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded values.</returns>
-        IEnumerable<TValue> TraversePostOrder();
-        /// <summary>Traverses the subtree with this node as a root using level-order.</summary>
-        /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded values.</returns>
-        IEnumerable<TValue> TraverseLevelOrder();
-        /// <summary>Traverses the subtree with this node as a root using pre-order.</summary>
         /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded nodes.</returns>
         IEnumerable<TTreeNode> TraversePreOrderNodes();
         /// <summary>Traverses the subtree with this node as a root using post-order.</summary>
@@ -109,10 +126,5 @@ namespace Garyon.DataStructures
         /// <returns>An <seealso cref="IEnumerable{T}"/> containing the yielded nodes.</returns>
         IEnumerable<TTreeNode> TraverseLevelOrderNodes();
         #endregion
-
-        /// <summary>Gets the tree view of this tree node as a subtree.</summary>
-        /// <param name="childIndent">The length of the indentation for each subsequent child.</param>
-        /// <returns>The tree view.</returns>
-        string GetTreeView(int childIndent = 4);
     }
 }
