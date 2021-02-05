e594e3bbb593a8996f24cf8e6d1f8799c62483a2
AlFas <AlFasGD@users.noreply.github.com>
2021-02-06 00:26:29 +0200

    Add a few tests for extensions

---
+++ Garyon.Tests/Extensions/IEnumerableExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Garyon.Extensions;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Garyon.Tests.Extensions
+{
+    public class IEnumerableExtensionsTests
+    {
+        [Test]
+        public void FlattenTest()
+        {
+            var numbers = new List<List<int>>
+            {
+                new List<int> { 0, 1, 2, 3 },
+                new List<int> { 4, 5 },
+                new List<int> { 6 },
+                new List<int> { 7, 8 },
+                new List<int> { 9, 10, 11, 12 },
+            };
+            int expectedLength = 0;
+            foreach (var list in numbers)
+                expectedLength += list.Count;
+
+            var flattened = numbers.Flatten().ToArray();
+            Assert.AreEqual(expectedLength, flattened.Length);
+
+            for (int i = 0; i < expectedLength; i++)
+                Assert.AreEqual(i, flattened[i]);
+        }
+    }
+}
---
+++ Garyon.Tests/Functions/EnumHelpersTests.cs
@@ -0,0 +1,22 @@
+﻿using Garyon.Functions;
+using NUnit.Framework;
+
+namespace Garyon.Tests.Functions
+{
+    public class EnumHelpersTests
+    {
+        [Test]
+        public void GetEntryCountTest()
+        {
+            Assert.AreEqual(3, EnumHelpers.GetEntryCount<TestEnum>());
+            Assert.AreEqual(3, EnumHelpers.GetEntryCount(typeof(TestEnum)));
+        }
+
+        private enum TestEnum
+        {
+            A,
+            B,
+            C,
+        }
+    }
+}
