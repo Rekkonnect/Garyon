04f34fdeb19b82e697f53fd2e46ac483eeb2e02f
AlFas <AlFasGD@users.noreply.github.com>
2021-02-06 14:42:59 +0200

    Dump benchmarks

---
+++ Garyon.Benchmarking/Mathematics/GeneralMath/MinMaxFunctions.cs
@@ -0,0 +1,65 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Linq;
+using static Garyon.Mathematics.GeneralMath;
+
+namespace Garyon.Benchmarking.Mathematics.General
+{
+    public class MinMaxFunctions
+    {
+        #region Byte
+        [Benchmark]
+        public void MinByteCustom()
+        {
+            byte min = Min((byte)1, (byte)4, (byte)12, (byte)0, (byte)5, (byte)15, (byte)3, (byte)19, (byte)255, (byte)123, (byte)131);
+        }
+        [Benchmark]
+        public void MinByteLINQ()
+        {
+            byte[] bytes = { 1, 4, 12, 0, 5, 15, 3, 19, 255, 123, 131 };
+            byte min = bytes.Min();
+        }
+        #endregion
+
+        #region Int16
+        [Benchmark]
+        public void MinInt16Custom()
+        {
+            short min = Min((short)1, (short)4, (short)12, (short)0, (short)5, (short)15, (short)3, (short)19, (short)255, (short)123, (short)131);
+        }
+        [Benchmark]
+        public void MinInt16LINQ()
+        {
+            short[] shorts = { 1, 4, 12, 0, 5, 15, 3, 19, 255, 123, 131 };
+            short min = shorts.Min();
+        }
+        #endregion
+
+        #region Int32
+        [Benchmark]
+        public void MinInt32Custom()
+        {
+            int min = Min(1, 4, 12, 0, 5, 15, 3, 19, 255, 123, 131);
+        }
+        [Benchmark]
+        public void MinInt32LINQ()
+        {
+            int[] ints = { 1, 4, 12, 0, 5, 15, 3, 19, 255, 123, 131 };
+            int min = ints.Min();
+        }
+        #endregion
+
+        #region Int64
+        [Benchmark]
+        public void MinInt64Custom()
+        {
+            long min = Min(1, 4, 12, 0, 5, 15, 3, 19, 255, 123, 131);
+        }
+        [Benchmark]
+        public void MinInt64LINQ()
+        {
+            long[] longs = { 1, 4, 12, 0, 5, 15, 3, 19, 255, 123, 131 };
+            long min = longs.Min();
+        }
+        #endregion
+    }
+}
---
+++ Garyon.Benchmarking/UsefulKnowledge/DictionaryInitializer.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+namespace Garyon.Benchmarking.UsefulKnowledge
+{
+    public class DictionaryInitializer
+    {
+        [Benchmark]
+        public void InitializeWithAdd()
+        {
+            var d = new Dictionary<int, int>
+            {
+                { 1, 2 },
+                { 2, 5 },
+                { 3, 8 },
+                { 4, 11 },
+            };
+        }
+
+        [Benchmark]
+        public void InitializeWithThisAccessor()
+        {
+            var d = new Dictionary<int, int>
+            {
+                [1] = 2,
+                [2] = 5,
+                [3] = 8,
+                [4] = 11,
+            };
+        }
+    }
+}
