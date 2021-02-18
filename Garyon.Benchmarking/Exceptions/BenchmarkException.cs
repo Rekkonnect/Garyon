c053ac1dc44b0c9ecb11143ba84830a321667975
AlFas <AlFasGD@users.noreply.github.com>
2021-02-18 20:09:16 +0200

    Minor code style adjustments

--- Garyon.Benchmarking/Exceptions/BenchmarkException.cs
+++ Garyon.Benchmarking/Exceptions/BenchmarkException.cs
@@ -4,8 +4,11 @@ namespace Garyon.Benchmarking.Exceptions
 {
     public class BenchmarkException : Exception
     {
-        public BenchmarkException() : base("An exception has occurred during a benchmark.") { }
-        public BenchmarkException(string message) : base(message) { }
-        public BenchmarkException(string message, Exception innerException) : base(message, innerException) { }
+        public BenchmarkException()
+            : base("An exception has occurred during a benchmark.") { }
+        public BenchmarkException(string message)
+            : base(message) { }
+        public BenchmarkException(string message, Exception innerException)
+            : base(message, innerException) { }
     }
 }
--- Garyon.Benchmarking/Exceptions/InstructionSetBenchmarkException.cs
+++ Garyon.Benchmarking/Exceptions/InstructionSetBenchmarkException.cs
@@ -4,8 +4,11 @@ namespace Garyon.Benchmarking.Exceptions
 {
     public class InstructionSetBenchmarkException : BenchmarkException
     {
-        public InstructionSetBenchmarkException() : base("The system does not support the required instruction set to test this function.") { }
-        public InstructionSetBenchmarkException(string message) : base(message) { }
-        public InstructionSetBenchmarkException(string message, Exception innerException) : base(message, innerException) { }
+        public InstructionSetBenchmarkException()
+            : base("The system does not support the required instruction set to test this function.") { }
+        public InstructionSetBenchmarkException(string message)
+            : base(message) { }
+        public InstructionSetBenchmarkException(string message, Exception innerException)
+            : base(message, innerException) { }
     }
 }
