e1ae0d332fff6c6fa2126c7e0825dfcbdfc4ccf4
Alex <kalfas.alex@gmail.com>
2022-12-26 13:04:06 +0200

    Add invokable type extensions

---
+++ Garyon/Reflection/FieldInfoExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace Garyon.Reflection;
+
+/// <summary>Contains extension methods for the <seealso cref="FieldInfo"/> class.</summary>
+public static class FieldInfoExtensions
+{
+    /// <summary>Determines the kind of the invokable type of a field.</summary>
+    /// <param name="field">The field whose type to evaluate.</param>
+    /// <returns>
+    /// The respective kind of invokable type that the field's type is.
+    /// <seealso cref="InvokableTypeKind.Invalid"/> will be returned if
+    /// the field type is not an invokable type.
+    /// </returns>
+    public static InvokableTypeKind GetFieldInvokableTypeKind(this FieldInfo field)
+    {
+        var type = field.FieldType;
+
+        // Subject (hopefully) to change in future versions of .NET
+        if (type == typeof(IntPtr))
+            return InvokableTypeKind.FunctionPointer;
+
+        if (type.IsDelegate())
+            return InvokableTypeKind.Delegate;
+
+        return InvokableTypeKind.Invalid;
+    }
+}
---
+++ Garyon/Reflection/InvokableTypeKind.cs
@@ -0,0 +1,18 @@
+﻿namespace Garyon.Reflection;
+
+/// <summary>
+/// Determines the kind of an invokable type. It could be a
+/// delegate, a function pointer, or not a valid invokable type.
+/// </summary>
+public enum InvokableTypeKind
+{
+    /// <summary>
+    /// Represents an invalid invokable type. It could be a type
+    /// that is not invokable, or an unsupported invokable type.
+    /// </summary>
+    Invalid,
+    /// <summary>Represents a delegate type.</summary>
+    Delegate,
+    /// <summary>Represents a function pointer type.</summary>
+    FunctionPointer
+}
