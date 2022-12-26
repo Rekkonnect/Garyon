a7fab3627dd8dffbf454d63f532116cb2ea3ab00
Alex <kalfas.alex@gmail.com>
2022-12-26 11:06:32 +0200

    Add tests and null check functions

---
+++ Garyon.Tests/Objects/Enumerators/AffixedEnumerableTests.cs
@@ -0,0 +1,43 @@
+﻿using Garyon.Objects.Enumerators;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Garyon.Tests.Objects.Enumerators;
+
+public class AffixedEnumerableTests
+{
+    [Test]
+    public void TestPrefix()
+    {
+        int[] main = { 1, 3, 458, 1239 };
+        var prefixed = main.WithPrefix(1, 2, 3);
+        var prefixedArray = prefixed.ToArray();
+        CollectionAssert.AreEqual(new[] { 1, 2, 3, 1, 3, 458, 1239 }, prefixedArray);
+    }
+    [Test]
+    public void TestSuffix()
+    {
+        int[] main = { 1, 3, 458, 1239 };
+        var suffixed = main.WithSuffix(1, 2, 3);
+        var suffixedArray = suffixed.ToArray();
+        CollectionAssert.AreEqual(new[] { 1, 3, 458, 1239, 1, 2, 3 }, suffixedArray);
+    }
+    [Test]
+    public void TestEmptyAffixes()
+    {
+        int[] main = { 1, 3, 458, 1239 };
+        var affixed = main.WithPrefix()
+                          .WithSuffix();
+        var suffixedArray = affixed.ToArray();
+        CollectionAssert.AreEqual(main, suffixedArray);
+    }
+    [Test]
+    public void TestAffix()
+    {
+        int[] main = { 1, 3, 458, 1239 };
+        var affixed = main.WithPrefix(4, 5, 6)
+                          .WithSuffix(1, 2, 3);
+        var suffixedArray = affixed.ToArray();
+        CollectionAssert.AreEqual(new[] { 4, 5, 6, 1, 3, 458, 1239, 1, 2, 3 }, suffixedArray);
+    }
+}
---
+++ Garyon.Tests/Objects/Enumerators/SingleOrEnumerableTests.cs
@@ -0,0 +1,57 @@
+﻿using Garyon.Objects.Enumerators;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Garyon.Tests.Objects.Enumerators;
+
+public class SingleOrEnumerableTests
+{
+    [Test]
+    public void TestSingle()
+    {
+        const int value = 3;
+        var instance = new SingleOrEnumerable<int>(value);
+        TestSingle(value, instance);
+    }
+    [Test]
+    public void TestEnumerable()
+    {
+        int[] enumerable = { 1, 2, 5, 67, 348 };
+        var instance = new SingleOrEnumerable<int>(enumerable);
+        TestEnumerable(enumerable, instance);
+    }
+
+    [Test]
+    public void TestAssign()
+    {
+        const int value = 3;
+        int[] enumerable = { 1, 2, 5, 67, 348 };
+
+        var instance = new SingleOrEnumerable<int>(value);
+        TestSingle(value, instance);
+
+        instance.Assign(enumerable);
+        TestEnumerable(enumerable, instance);
+
+        instance.Assign(value);
+        TestSingle(value, instance);
+    }
+
+    private void TestSingle<T>(T value, SingleOrEnumerable<T> instance)
+    {
+        Assert.Catch(() => _ = instance.Enumerable);
+        Assert.AreEqual(value, instance.Single);
+
+        var enumeratedArray = instance.ToArray();
+        CollectionAssert.AreEqual(new[] { value }, enumeratedArray);
+    }
+    private void TestEnumerable<T>(IEnumerable<T> enumerable, SingleOrEnumerable<T> instance)
+    {
+        Assert.Catch(() => _ = instance.Single);
+        CollectionAssert.AreEqual(enumerable, instance.Enumerable);
+
+        var enumeratedArray = instance.ToArray();
+        CollectionAssert.AreEqual(enumerable, enumeratedArray);
+    }
+}
