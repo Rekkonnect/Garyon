47aabf1e9dd72d842da47eb03f80acc3f822f717
Alex <kalfas.alex@gmail.com>
2023-01-07 11:13:27 +0200

    Some prework on function pointer improvements

---
+++ Garyon/Reflection/FunctionPointer.cs
@@ -0,0 +1,25 @@
+﻿namespace Garyon.Objects.FunctionPointers;
+
+// TODO: Autogenerate those
+public unsafe readonly record struct FunctionPointerAction(delegate*<void> Pointer)
+{
+    public void Invoke() => Pointer();
+}
+public unsafe readonly record struct FunctionPointerAction<T1>(delegate*<T1, void> Pointer)
+{
+    public void Invoke(T1 arg1) => Pointer(arg1);
+}
+
+public unsafe readonly record struct FunctionPointer<TReturn>(delegate*<TReturn> Pointer)
+{
+    public TReturn Invoke() => Pointer();
+}
+public unsafe readonly record struct FunctionPointer<T1, TReturn>(delegate*<T1, TReturn> Pointer)
+{
+    public TReturn Invoke(T1 arg1) => Pointer(arg1);
+}
+
+public unsafe readonly record struct UnmanagedFunctionPointerAction(delegate* unmanaged<void> Pointer)
+{
+    public void Invoke() => Pointer();
+}
